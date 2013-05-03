ec6164dc79e8a13b0d984e1d2409e90bb0894c4a
root <root@AY120725112408fc44792.(none)>
2013-05-03 12:24:11 +0800

    delete *.sh

--- gfStock/shell/start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
- setLocal EnableDelayedExpansion
- set CLASSPATH="
- for /R ./libs %%a in (*.jar) do (
-   set CLASSPATH=!CLASSPATH!;%%a
- )
- set CLASSPATH=!CLASSPATH!"
- echo %CLASSPATH%
- java -cp classes;%CLASSPATH% job.gf.stock.Runner
--- gfStock/shell/stop.sh
+++
@@ -1,9 +0,0 @@
-if [ -f 'pid' ];then
-PID=`cat pid`
-if kill -9 $PID;then
-echo "kill sucess"
-else
-echo "kill fail"
-fi
-rm -f 'pid'
-fi
